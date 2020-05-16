8e618e6ce685230a83f2b7245cb2c34bc433be76
Secretary-bird <LLopez31098@gmail.com>
2020-05-16 00:45:47 -0400

    Finished the Multi assignment

---
+++ Assets/New Scripts/Collection.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class Collection : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Ups"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+
+        countText.text = "Count: " + count.ToString();
+        if (count > 5)
+        {
+            winText.text = "Now if only you could escape.";
+        }
+    }
+}
---
+++ Assets/New Scripts/Rotator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class Rotator : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Ups"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+
+        countText.text = "Count: " + count.ToString();
+        if (count >= 6)
+        {
+            winText.text = "Now to escape";
+        }
+    }
+}
